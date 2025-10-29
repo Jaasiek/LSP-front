c82efb7d41d246be2100811fb18e743ebe402631
Anti0I <u125_antkmu_waw@technischools.com>
2025-10-29 14:38:09 +0100

    mapa-poczatkowa-test

--- src/components/map/map.jsx
+++ src/components/map/map.jsx
@@ -1,7 +1,33 @@
+import React from "react";
+import { GoogleMap, Marker, useJsApiLoader } from "@react-google-maps/api";
+
+const containerStyle = {
+  width: "100%",
+  height: "500px",
+};
+
+const center = {
+  lat: 52.2297,
+  lng: 21.0122,
+};
+
 export default function Map() {
+  const { isLoaded } = useJsApiLoader({
+    id: "google-map-script",
+    googleMapsApiKey: "AIzaSyBCXH8M2htOaqMfhLSn55G2nXj1x7FrXIQ"
+  });
+  if (!isLoaded) {
+    return <p>Ładowanie mapy...</p>;
+  }
+
+
   return (
-    <div className="map">
-      <h2>MAP</h2>
-    </div>
+    <GoogleMap
+      mapContainerStyle={containerStyle}
+      center={center}
+      zoom={12}
+    >
+      <Marker position={center} title="Warszawa" />
+    </GoogleMap>
   );
 }
